4288978269686f86a2d116c172f950128493c283
ConstanceMorel <morelconstance@gmail.com>
2016-12-24 11:32:14 +0100

    Version 1.1.0

--- check_ssl.sh
+++ check_ssl.sh
@@ -1,18 +1,61 @@
 #!/bin/bash
 
-FQDN_SERVER=$1
 TMP_HEAD=/tmp/head.tmp
 VERT="\\033[1;32m"
 NORMAL="\\033[0;39m"
 ROUGE="\\033[1;31m"
-ROSE="\\033[1;35m"
-BLEU="\\033[1;34m"
-BLANC="\\033[0;02m"
-BLANCLAIR="\\033[1;08m"
 JAUNE="\\033[1;33m"
-CYAN="\\033[1;36m"
+VERSION=1.1.0
 
-curl https://${FQDN_SERVER}/ -ks -I -o $TMP_HEAD
+MAIN_HTTP_METHODS='
+GET
+HEAD
+OPTIONS
+DELETE
+PUT
+CONNECT'
+
+OPTIONAL_HTTP_METHODS='
+TRACK
+CUSTOM'
+
+usage() {
+    echo "$(basename "${0}")"' [-hv] -f <fqdn>'
+    echo ""
+}
+
+if [ -z "${1}" ]; then
+    usage >&2
+    exit 1
+fi
+
+while getopts "f:hv" opt; do
+  case $opt in
+    f)
+      FQDN_SERVER=$OPTARG
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+    v)
+      echo 'Version '"${VERSION}"
+      exit 0
+      ;;
+    \?)
+      usage >&2
+      exit 1
+      ;;
+    :)
+      usage >&2
+      exit 1
+      ;;
+  esac
+done
+
+trap 'rm -f "${TMP_HEAD}"' EXIT
+
+curl https://"${FQDN_SERVER}"/ -ks -I -o $TMP_HEAD
 
 # HSTS
 # max-age=31536000; includeSubdomains; preload
@@ -34,7 +77,7 @@ fi
 
 # ALPN
 # listen 443 ssl http2;
-ALPN=$(echo -n | openssl s_client -alpn h2 -connect ${FQDN_SERVER}:443 2>&1 | grep ALPN)
+ALPN=$(echo -n | openssl s_client -alpn h2 -connect "${FQDN_SERVER}":443 2>&1 | grep ALPN)
 if [ "${ALPN}" == "ALPN protocol: h2" ]; then
     echo -e "${JAUNE}ALPN   ${VERT}present (OK)${NORMAL}"
 else
@@ -42,7 +85,7 @@ else
 fi
 
 # NPN
-NPN=$(echo -n | openssl s_client -nextprotoneg h2 -connect ${FQDN_SERVER}:443 2>&1 | grep "Next protocol")
+NPN=$(echo -n | openssl s_client -nextprotoneg h2 -connect "${FQDN_SERVER}":443 2>&1 | grep "Next protocol")
 if [ "${NPN}" == "Next protocol: (1) h2" ]; then
     echo -e "${JAUNE}NPN    ${VERT}present (OK)${NORMAL}"
 else
@@ -50,19 +93,19 @@ else
 fi
 
 
-for http in $(echo GET HEAD OPTIONS DELETE PUT CONNECT)
+for http in ${MAIN_HTTP_METHODS}
 do
-    RET=$(curl -ks https://${FQDN_SERVER}/ -I -X ${http} | head -1 | grep 200)
+    RET=$(curl -ks https://"${FQDN_SERVER}"/ -I -X "${http}" | head -1 | grep 200)
     if [ -n "${RET}" ]; then
         echo -e "${JAUNE}HTTP Method : ${http}  ${VERT}joignable (OK)${NORMAL}"
     else
-        echo -e "${JAUNE}HTTP Method : ${http}  ${JAUNE}injoignable (OK)${NORMAL}"
+        echo -e "${JAUNE}HTTP Method : ${http}  ${VERT}injoignable (OK)${NORMAL}"
     fi
 done
 
-for http in $(echo TRACK CUSTOM)
+for http in ${OPTIONAL_HTTP_METHODS}
 do
-    RET=$(curl -ks https://${FQDN_SERVER}/ -I -X ${http} | head -1 | grep 200)
+    RET=$(curl -ks https://"${FQDN_SERVER}"/ -I -X "${http}" | head -1 | grep 200)
     if [ -z "${RET}" ]; then
         echo -e "${JAUNE}HTTP Method : ${http}  ${VERT}injoignable (OK)${NORMAL}"
     else
